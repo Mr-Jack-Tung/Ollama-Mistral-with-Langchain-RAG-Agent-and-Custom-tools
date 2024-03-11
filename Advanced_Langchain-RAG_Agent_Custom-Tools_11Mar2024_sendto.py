5c30b15a78cb74866542700d46516bf6242a8a5a
Mr. Jack Tung <72214141+Mr-Jack-Tung@users.noreply.github.com>
2024-03-11 12:19:48 +0700

    Add files via upload

---
+++ Advanced_Langchain-RAG_Agent_Custom-Tools_11Mar2024_sendto.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# Author: Mr.Jack _ www.BICweb.vn
+# Start: 03Mar2024 - 09PM
+# End: 11Mar2024 - 12PM
+
+from langchain.agents import AgentExecutor, create_react_agent, load_tools
+from langchain.prompts import PromptTemplate
+from langchain.memory import ConversationBufferWindowMemory
+
+from langchain_community.llms import Ollama
+model_local = Ollama(model='mistral')
+
+from simple_langchain_tools import get_all_tools
+
+tools = get_all_tools()
+
+from langchain import hub
+prompt = hub.pull("hwchase17/react-chat")
+
+chat_history_memory = ConversationBufferWindowMemory(k=3, memory_key='chat_history', input_key='input', ouput_key='output')
+
+agent = create_react_agent(model_local, tools, prompt)
+
+agent_executor = AgentExecutor(
+    agent=agent, 
+    tools=tools, 
+    memory=chat_history_memory,
+    verbose=True, # ~> Speech out the thinking
+    # return_intermediate_steps=True, # ~> Return thinking process
+    handle_parsing_errors=True, 
+    # return_only_outputs=True # ~> Return only outputs for next step input
+    )
+
+user_input =""
+while user_input != 'exit!':
+    # print("\ntools:",tools)
+    user_input = input('\n(type exit! to quit) Ask > ')
+    # print("\nuser_input:", user_input)
+
+    if user_input != 'exit!':
+        response = agent_executor.invoke({"input": user_input})
+
+        print("\n")
+        # print(response)
+        print("Question:",response['input'])
+        print("Answer:",response['output'])
+        print("\n")
